151e17fecae15611dcd54ca5891bf93ee3ffee74
Ezer Silva <ezersilva@gmail.com>
2026-02-27 21:08:53 -0300

    fix bug

--- cmd/repoinjector/main.go
+++ cmd/repoinjector/main.go
@@ -1,7 +1,7 @@
 // Package main is the entry point for the repoinjector CLI.
 package main
 
-import "github.com/ezer/repoinjector/internal/cmd"
+import "github.com/ezerfernandes/repoinjector/internal/cmd"
 
 func main() {
 	cmd.Execute()
